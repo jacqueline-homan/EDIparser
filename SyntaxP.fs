1ae8a1a9ccf762e9d3483d7fbf6f2bf91b321ee6
Evan Cofsky <evan@theunixman.com>
2016-12-14 17:57:09 -0600

    SyntaxP now parses the chain of ISA with Auth Info.

--- SyntaxP.fs
+++ SyntaxP.fs
@@ -18,6 +18,8 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+module EDIParser.SyntaxP
+
 open FParsec
 
 // Parse a record like:
@@ -25,9 +27,37 @@ open FParsec
 //     ISA*00*          *00*          *ZZ*MGCTLYST       *02*BLNJ           *160930*1453*U*00401*000000001*0*P*:
 //
 // Into a record type tag and then a sequence of fields
-type ISA = ISA
+type Parser<'t> = Parser<'t, unit>
 
-let pISA <ISA, 'u> = stringReturn "ISA" ISA
+// The field separator
+let pFSep : Parser<_> = skipChar '*'
+
+// The Authorization Qualifier
+type AuthQual = AQNone
+
+// We only have one of these anyway
+let pAuthQual : Parser<AuthQual> = stringReturn "00" AQNone .>> pFSep
+
+type AuthInfo = 
+    | AuthInfo of string
+
+let pAuthInfo : Parser<AuthInfo> = anyString 10 |>> AuthInfo .>> pFSep
+
+// The Authorization Qualifier and Info
+type Auth = 
+    { authQual : AuthQual
+      authInfo : AuthInfo }
+
+let pAuth = 
+    pipe2 pAuthQual pAuthInfo (fun q i -> 
+        { authQual = q
+          authInfo = i })
+
+type ISA = 
+    | ISA of Auth
+
+let pISARec : Parser<_> = skipString "ISA" >>. pFSep
+let pISA : Parser<ISA> = pISARec >>. pAuth |>> ISA
 
 let test p str = 
     match run p str with
